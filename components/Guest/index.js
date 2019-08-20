5507abfbccc9c62746608e969997c94b7976c6aa
antonk52 <halloy52@gmail.com>
2019-08-21 00:23:38 +0300

    feat(lightkeeper): optimize images

--- components/Guest/index.js
+++ components/Guest/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import styled from 'reshadow';
 import styles from './style.css';
 
-const Guest = ({src, shade = 'dark', name, ...props}) => styled(styles)(
+const Guest = ({img, shade = 'dark', name, ...props}) => styled(styles)(
     <guest as="figure" use:shade={shade} {...props}>
-        <img src={src} alt={name} />
+        <img src={img.src} srcSet={img.srcSet} alt={name} />
         <name as="figcaption">{name}</name>
     </guest>
 );
