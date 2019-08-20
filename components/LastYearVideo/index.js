a4e4e0a2854a817ff2e862b9d332c6d26c421b7d
antonk52 <halloy52@gmail.com>
2019-08-21 01:57:23 +0300

    feat(lightkeeper): delay vimeo download

--- components/LastYearVideo/index.js
+++ components/LastYearVideo/index.js
@@ -10,18 +10,36 @@ const Title = () => <span>
     <Text color="accent">the</Text> bowl <Text color="accent">4</Text>
 </span>;
 
-const LastYearVideo = () => styled(styles)(
-    <Section title={<Title />}>
-        <videoWrapper>
-            <iframe
-                src="https://player.vimeo.com/video/340422924"
-                style={{position:'absolute',top:0,left:0,border: 0,width:'100%',height:'100%'}}
-                allow="autoplay; fullscreen"
-                allowFullScreen
-                title="видео отчет the bowl 4"
-            />
-        </videoWrapper>
-    </Section>
-);
+const LastYearVideo = () => {
+    const [shouldRender, onRenderChange] = React.useState(false);
+    React.useEffect(() => {
+        if (window.scrollY > 0) {
+            onRenderChange(true);
+            return;
+        }
+        window.addEventListener('scroll', () => onRenderChange(true), {
+            once: true,
+        });
+    });
+
+    return styled(styles)(
+        <Section title={<Title />}>
+            <videoWrapper>
+                {
+                    shouldRender
+                        ? (
+                            <iframe
+                                src="https://player.vimeo.com/video/340422924"
+                                style={{position:'absolute',top:0,left:0,border: 0,width:'100%',height:'100%'}}
+                                allow="autoplay; fullscreen"
+                                allowFullScreen
+                                title="видео отчет the bowl 4"
+                            />
+                        ) : null
+                }
+            </videoWrapper>
+        </Section>
+    )
+};
 
 export default LastYearVideo;
